c95d0b333654106adba5589a8c224dc01495cf1f
shourovr82 <shafinur512@gmail.com>
2024-10-09 12:17:57 +0600

    [DONE] even odd positive and negative

---
+++ EvenOddPositiveNegative.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+
+{
+  int n;
+  scanf("%d", &n);
+
+  int evenCount = 0,
+      oddCount = 0,
+      positiveCount = 0,
+      negativeCount = 0;
+
+  for (int i = 0; i < n; i++)
+  {
+    int x;
+    scanf("%d", &x);
+    if (x % 2 == 0)
+    {
+      // even
+      evenCount++;
+    }
+    else
+    {
+      // odd
+      oddCount++;
+    }
+    if (x > 0)
+    {
+      // positive
+      positiveCount++;
+    }
+    else if (x < 0)
+    {
+      // negative
+      negativeCount++;
+    }
+  }
+
+  printf("Even: %d\n", evenCount);
+  printf("Odd: %d\n", oddCount);
+  printf("Positive: %d\n", positiveCount);
+  printf("Negative: %d\n", negativeCount);
+
+  return 0;
+}
